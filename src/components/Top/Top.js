60f3506a87d552d26c19cb4880e8b44cba2d5578
DevSujal <nimjesujal2004@gmail.com>
2024-06-07 17:29:28 +0530

    "Updated Top component CSS and JS: changed flex values, font sizes, and margins; removed console log; updated image width and height."

--- src/components/Top/Top.js
+++ src/components/Top/Top.js
@@ -6,14 +6,12 @@ const Top = ({
   return (
     <div className="top">
       <div className="left">
-        {
-          console.log(data)}
         <span className="city">{data ? data.location.name : "Nagpur"}</span>
         <span>humidity {data ? data.current.humidity : 40}</span>
         <span className="city">{data ? data.current.temp_c : 30}&deg; C</span>
       </div>
       <div className="right">
-        {data ? <img src={data.current.condition.icon} width={300} height={300} alt="icon" /> : <MdSunny color="yellow" />}
+        {data ? <img src={data.current.condition.icon} width={200} height={200} alt="icon" /> : <MdSunny color="yellow" />}
       </div>
     </div>
   );
