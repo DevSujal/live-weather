9d624dc1ed38e8fee2c04fe0efb403a51be15e12
DevSujal <nimjesujal2004@gmail.com>
2024-06-27 08:53:46 +0530

    enhanced the weather by putting suggestions and beutifying the css

--- src/App.js
+++ src/App.js
@@ -12,34 +12,62 @@ function App() {
   const myRef = useRef();
 
   const [city, setCity] = useState("");
-  const [data, setData] = useState()
+  const [data, setData] = useState();
   useEffect(() => {
-     myRef.current.addEventListener("keypress",(e) => {
-      if (e.key === "Enter") {
-        axios.get(
-          `https://api.weatherapi.com/v1/current.json?key=b13989793f184149a91141538230103&q=${e.target.value}`
-        ).then((response) => {
-          setData(() => {
-            return response.data
-          })
-        }).catch((err)=> {
-          console.log("error occured", err?.message)
-        }).finally(() =>{
-          setCity("")
-        })
-      }
-    });
+    axios
+      .get(
+        `https://api.weatherapi.com/v1/current.json?key=b13989793f184149a91141538230103&q=nagpur`
+      )
+      .then((response) => {
+        setData(() => {
+          return response.data;
+        });
+      })
+      .catch((err) => {
+        console.log("error occured", err?.message);
+      })
+      .finally(() => {
+        setCity("");
+      });
   }, []);
 
+  myRef.current?.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+      axios
+        .get(
+          `https://api.weatherapi.com/v1/current.json?key=b13989793f184149a91141538230103&q=${e.target.value}`
+        )
+        .then((response) => {
+          setData(() => {
+            return response.data;
+          });
+        })
+        .catch((err) => {
+          console.log("error occured", err?.message);
+        })
+        .finally(() => {
+          setCity("");
+        });
+    }
+  });
+
   return (
-    
     <div className="app">
       <Sidebar />
       <div className="main-container">
-        <Search myRef={myRef} setCity = {setCity} city={city} />
-        <Top data = {data}/>
-        <Middle />
-        <Footer data = {data}/>
+        <Search ref={myRef} setCity={setCity} city={city} />
+        <Top data={data} />
+        {window.innerWidth <= 640 ? (
+          <>
+            <Footer data={data} />
+            <Middle />
+          </>
+        ) : (
+          <>
+            <Middle />
+            <Footer data={data} />
+          </>
+        )}
       </div>
       <WeatherInfo />
     </div>
